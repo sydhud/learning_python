34d57e1eae284e673061c1e9f607b3419ac4ebd5
Sydney Huddleston <75635879+sydhud@users.noreply.github.com>
2024-03-27 11:29:17 -0500

    Create password_generator.py

---
+++ password_project/password_generator.py
@@ -0,0 +1,8 @@
+import random
+print('Your password: ')
+chars = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()?'
+password = ''
+for x in range(16):
+    password += random.choice(chars)
+
+print(password)
